02e1a1e284de16d98ca25776c9e450b2a934acf1
endalk200 <eb808826@gmail.com>
2024-07-08 18:12:42 +0300

    feat: added AuthMiddleware and LoggingMiddleware with createStack utility function

---
+++ middlewares.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	jwtutils "github.com/endalk200/http-server/utils"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+func CreateStack(xs ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for i := len(xs) - 1; i >= 0; i-- {
+			x := xs[i]
+			next = x(next)
+		}
+
+		return next
+	}
+}
+
+const AuthUserId = "middleware.auth.userID"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := strings.Split(r.Header.Get("Authorization"), " ")[1]
+		if tokenString == "" {
+			log.Printf("Unauthorized API call. Authorization token is not provided")
+			http.Error(w, "Unauthorized API call", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwtutils.VerifyJWT(tokenString)
+		if err != nil {
+			log.Printf("Invalid JWT is provided: %s", err)
+			http.Error(w, "Unauthorized API call", http.StatusUnauthorized)
+			return
+		}
+
+		userId, _ := token.Claims.GetSubject()
+		ctx := context.WithValue(r.Context(), AuthUserId, userId)
+		req := r.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
+	})
+}
+
+type WrappedWritter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *WrappedWritter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		wrapped := &WrappedWritter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+
+		log.Println(wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
+	})
+}
