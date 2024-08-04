224883345150c36f18780f8e6e25b8c81bafb03a
endalk200 <eb808826@gmail.com>
2024-08-04 17:42:13 +0300

    config: updated network configuration specifically the security_group for azure

--- infrastructure/azure/network.tf
+++ infrastructure/azure/network.tf
@@ -40,6 +40,32 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  # Allow inbound HTTP connections
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow inbound HTTPS connections
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 output "public_ip_address" {
