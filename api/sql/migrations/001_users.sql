eb56217b6da441bbd09cbb3d33511417c60a2b8a
endalk200 <eb808826@gmail.com>
2024-08-12 16:06:13 +0300

    feat: added SQL code for user database table

---
+++ api/sql/migrations/001_users.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE users (
+  id   SERIAL,
+
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+
+  password VARCHAR(255) NOT NULL,
+	email VARCHAR(320) UNIQUE NOT NULL,
+	is_email_verified BOOLEAN DEFAULT FALSE,
+	is_active BOOLEAN DEFAULT TRUE,
+	github_handle VARCHAR(255),
+
+  PRIMARY KEY(id)
+);
+
+-- +goose Down 
+DROP TABLE users; 
