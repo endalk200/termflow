26b4df620496e49ebfecdcdd8c24ff9dc41d4247
endalk200 <eb808826@gmail.com>
2024-09-26 21:07:19 +0300

    feat: adds database migration and SQL queries powering sqlc function generation
    - Commands table
    - Tags table
    - Command related sqlc queries
    - Tags related sqlc queries

--- cli/sql/migrations/001_init.sql
+++ cli/sql/migrations/001_init.sql
@@ -1,17 +1,26 @@
 -- +goose Up
-CREATE TABLE Collection (
+CREATE TABLE Tag (
   id   INTEGER PRIMARY KEY,
-  name text    NOT NULL,
+  name text    NOT NULL UNIQUE,
   description  text
 );
 
 CREATE TABLE Command (
   id INTEGER PRIMARY KEY,
-  name text NOT NULL,
-  description text,
-  command text
-)
+  command text,
+  description text
+);
+
+CREATE TABLE CommandTag (
+  commandId INTEGER,
+  tagId INTEGER,
+
+  PRIMARY KEY (commandId, tagId),
+  FOREIGN KEY (commandId) REFERENCES Command(id) ON DELETE CASCADE,
+  FOREIGN KEY (tagId) REFERENCES Tag(id) ON DELETE CASCADE
+);
 
 -- +goose Down 
-DROP TABLE Collection;
+DROP TABLE Tag;
 DROP TABLE Command;
+DROP TABLE CommandTag;
--- cli/sql/queries/command.sql
+++ cli/sql/queries/command.sql
@@ -4,22 +4,104 @@ WHERE id = ? LIMIT 1;
 
 -- name: ListCommands :many
 SELECT * FROM Command
-ORDER BY name;
+ORDER BY id;
 
--- name: AddCommand :exec
+-- name: ListCommandsForTagByName :many
+SELECT c.id, c.command, c.description
+FROM Command c
+JOIN CommandTag ct ON c.id = ct.commandId
+JOIN Tag t ON ct.tagId = t.id
+WHERE t.name = ?;
+
+-- name: ListCommandsWithTagsByTagName :many
+SELECT 
+  c.id AS command_id, 
+  c.command, 
+  c.description AS command_description,
+  t.id AS tag_id, 
+  t.name AS tag_name, 
+  t.description AS tag_description
+FROM Command c
+LEFT JOIN CommandTag ct ON c.id = ct.commandId
+LEFT JOIN Tag t ON ct.tagId = t.id
+WHERE t.name = ?
+ORDER BY c.id, t.name;
+
+-- name: ListCommandsWithTags :many
+SELECT 
+    c.id AS command_id, 
+    c.command, 
+    c.description AS command_description,
+    t.id AS tag_id, 
+    t.name AS tag_name, 
+    t.description AS tag_description
+FROM Command c
+LEFT JOIN CommandTag ct ON c.id = ct.commandId
+LEFT JOIN Tag t ON ct.tagId = t.id
+ORDER BY c.id, t.name;
+
+-- name: AddCommand :one
 INSERT INTO Command (
-  name, description, command
+  command, description
 ) VALUES (
-  ?, ?, ?
-);
+  ?, ?
+) RETURNING *;
 
 -- name: UpdateCommand :exec
 UPDATE Command
-set name = ?,
-description = ?,
-command = ?
+set command = ?,
+description = ?
 WHERE id = ?;
 
 -- name: DeleteCommand :exec
 DELETE FROM Command
 WHERE id = ?;
+
+-- name: GetTag :one
+SELECT * FROM Tag
+WHERE id = ? LIMIT 1;
+
+-- name: GetTagByName :one
+SELECT * FROM Tag
+WHERE name = ? LIMIT 1;
+
+-- name: ListTags :many
+SELECT * FROM Tag
+ORDER BY name;
+
+-- name: AddTag :one
+INSERT INTO Tag (
+  name, description
+) VALUES (
+  ?, ?
+) RETURNING *;
+
+-- name: UpdateTag :exec
+UPDATE Tag
+SET name = ?,
+description = ?
+WHERE id = ?;
+
+-- name: DeleteTag :exec
+DELETE FROM Tag
+WHERE id = ?;
+
+-- name: AddCommandTag :exec
+INSERT INTO CommandTag (
+  commandId, tagId
+) VALUES (
+  ?, ?
+);
+
+-- name: RemoveCommandTag :exec
+DELETE FROM CommandTag
+WHERE commandId = ? AND tagId = ?;
+
+-- name: GetTagsForCommand :many
+SELECT t.id, t.name, t.description
+FROM Tag t
+JOIN CommandTag ct ON t.id = ct.tagId
+WHERE ct.commandId = ?;
+
+
+
